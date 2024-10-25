a57c1156f241c4900fb7d1099986651dffad88c6
Hirofumi Seiyama <hseiyama@gmail.com>
2024-10-26 01:44:26 +0900

    NUCLEO-C071RB_hal01: RTCの更新機能を追加

--- HalProjects/NUCLEO-C071RB_hal01/Core/Src/main_app.c
+++ HalProjects/NUCLEO-C071RB_hal01/Core/Src/main_app.c
@@ -20,6 +20,7 @@
 #define CRC_DATA_ADDR0		(0x00000000)			/* CRC演算データアドレス0	*/
 #define CRC_DATA_ADDR1		(0x08000000)			/* CRC演算データアドレス1	*/
 #define CRC_DATA_SIZE		(48)					/* CRC演算データサイズ		*/
+#define RTC_STR_SIZE		(8)						/* RTC文字列サイズ			*/
 
 /* Private macro -------------------------------------------------------------*/
 
@@ -40,11 +41,20 @@ static uint16_t u16s_PwmDutyValue_prev;						/* PWMデューティ値(前回値)
 static uint8_t u8s_MessageData[MESSAGE_SIZE + 1];			/* メッセージデータ			*/
 static uint16_t u16s_MessageCount;							/* メッセージカウント		*/
 static uint8_t u8s_MessageUpdate;							/* メッセージ更新フラグ		*/
+static uint8_t u8s_RtcStrData[RTC_STR_SIZE];				/* RTC文字列データ			*/
+static uint16_t u16s_RtcStrCount;							/* RTC文字列カウント		*/
+static uint8_t u8s_RtcDateUpdate;							/* RTC日付更新フラグ		*/
+static uint8_t u8s_RtcTimeUpdate;							/* RTC時刻更新フラグ		*/
 
 /* Private function prototypes -----------------------------------------------*/
 static void getFlashData(void);								/* FLASHデータを取得する				*/
 static void setFlashData(void);								/* FLASHデータを更新する				*/
 static void showRtcTime(void);								/* RTC時間を表示する					*/
+static void showRtcDateTime(void);							/* RTC日時を表示する					*/
+static void updateRtcDate(uint8_t *pu8_BcdData);			/* RTC日付を更新する					*/
+static void updateRtcTime(uint8_t *pu8_BcdData);			/* RTC時刻を更新する					*/
+static uint8_t convStrToBcd(const uint8_t *ps8_StrData, uint16_t u16_StrLen, uint8_t *pu8_BcdData);
+															/* 文字列をBCDデータに変換する			*/
 
 /* Exported functions --------------------------------------------------------*/
 
@@ -72,12 +82,16 @@ void setup(void)
 	mem_set16(&u16s_AdcData[0], ADC_FAILURE_VALUE, ADC_CHANNEL_MAX);
 	mem_set08(&u8s_FlashDataBuffer[0], 0x00, FLASH_DATA_SIZE);
 	mem_set08(&u8s_MessageData[0], 0x00, sizeof(u8s_MessageData));
+	mem_set08(&u8s_RtcStrData[0], 0x00, RTC_STR_SIZE);
 	u8s_DisplayState = ON;
 	u16s_PwmDutyValue = 0;
 	u16s_PwmDutyValue_prev = 0;
 	u16s_MessageCount = 0;
 	u8s_MessageUpdate = OFF;
 	u8s_Exti0Event = OFF;
+	u16s_RtcStrCount = 0;
+	u8s_RtcDateUpdate = OFF;
+	u8s_RtcTimeUpdate = OFF;
 
 	/* UART送信バッファに改行コードをセット */
 	u8s_TxBuffer[UART_RX_BLOCK_SIZE] = '\r';			/* CRコード					*/
@@ -109,6 +123,7 @@ void loop(void)
 	uint8_t u8_I2cData;
 	uint8_t u8_SpiData;
 	uint32_t u32_CrcValue;
+	uint8_t u8_RtcBcdData[RTC_STR_SIZE / 2];
 
 	/* UART受信データの数を取得する */
 	if (uartGetRxCount() >= UART_RX_BLOCK_SIZE) {
@@ -128,6 +143,36 @@ void loop(void)
 			if (u16s_MessageCount >= MESSAGE_SIZE) {
 				u16s_MessageCount = 0;
 				u8s_MessageUpdate = OFF;
+			}
+		}
+		/* RTC日付更新フラグがON、またはRTC時刻更新フラグがONの場合 */
+		if ((u8s_RtcDateUpdate == ON) || (u8s_RtcTimeUpdate == ON)) {
+			if (u16s_RtcStrCount < RTC_STR_SIZE) {
+				mem_cpy08(&u8s_RtcStrData[u16s_RtcStrCount], &u8s_RxBuffer[0], u16_RxSize);
+				u16s_RtcStrCount += u16_RxSize;
+			}
+			if (u16s_RtcStrCount >= RTC_STR_SIZE) {
+				u16s_RtcStrCount = 0;
+				/* RTC日付更新フラグがONの場合 */
+				if (u8s_RtcDateUpdate == ON) {
+					u8s_RtcDateUpdate = OFF;
+					/* 文字列をBCDデータに変換する */
+					if (convStrToBcd(&u8s_RtcStrData[0], RTC_STR_SIZE, &u8_RtcBcdData[0]) == OK) {
+						/* RTC日付を更新する */
+						updateRtcDate(u8_RtcBcdData);
+					}
+				}
+				/* RTC時刻更新フラグがONの場合 */
+				if (u8s_RtcTimeUpdate == ON) {
+					u8s_RtcTimeUpdate = OFF;
+					/* 文字列をBCDデータに変換する */
+					if (convStrToBcd(&u8s_RtcStrData[0], RTC_STR_SIZE, &u8_RtcBcdData[0]) == OK) {
+						/* RTC時刻を更新する */
+						updateRtcTime(u8_RtcBcdData);
+					}
+				}
+				/* RTC日時を表示する */
+				showRtcDateTime();
 			}
 		}
 		/* 表示状態の切替え(OFF) */
@@ -186,6 +231,20 @@ void loop(void)
 				HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
 				HAL_Delay(125);
 			}
+		}
+		/* RTC日時を表示する */
+		else if (mem_cmp08(&u8s_RxBuffer[0], (uint8_t *)"rtcr", UART_RX_BLOCK_SIZE) == 0) {
+			showRtcDateTime();
+		}
+		/* RTC日付を更新する */
+		else if (mem_cmp08(&u8s_RxBuffer[0], (uint8_t *)"rtcd", UART_RX_BLOCK_SIZE) == 0) {
+			u16s_RtcStrCount = 0;
+			u8s_RtcDateUpdate = ON;
+		}
+		/* RTC時刻を更新する */
+		else if (mem_cmp08(&u8s_RxBuffer[0], (uint8_t *)"rtct", UART_RX_BLOCK_SIZE) == 0) {
+			u16s_RtcStrCount = 0;
+			u8s_RtcTimeUpdate = ON;
 		}
 		/* CRC演算を実行する(データアドレス0) */
 		else if (mem_cmp08(&u8s_RxBuffer[0], (uint8_t *)"crc0", UART_RX_BLOCK_SIZE) == 0) {
@@ -350,4 +409,112 @@ static void showRtcTime(void)
 	uartEchoHex8(st_RtcTime.Minutes);
 	uartEchoStr(":");
 	uartEchoHex8(st_RtcTime.Seconds);
+}
+
+/**
+  * @brief RTC日時を表示する
+  * @param None
+  * @retval None
+  */
+static void showRtcDateTime(void)
+{
+	RTC_TimeTypeDef st_RtcTime;
+	RTC_DateTypeDef st_RtcDate;
+
+	/* RTC現在時間を取得 */
+	HAL_RTC_GetTime(&hrtc, &st_RtcTime, RTC_FORMAT_BCD);
+	/* RTC現在日付を取得 */
+	HAL_RTC_GetDate(&hrtc, &st_RtcDate, RTC_FORMAT_BCD);
+
+	/* RTC現在日時を表示する */
+	uartEchoStr("RTC = 20");
+	uartEchoHex8(st_RtcDate.Year);
+	uartEchoStr("/");
+	uartEchoHex8(st_RtcDate.Month);
+	uartEchoStr("/");
+	uartEchoHex8(st_RtcDate.Date);
+	uartEchoStr("(");
+	uartEchoHex8(st_RtcDate.WeekDay);
+	uartEchoStr(") ");
+	uartEchoHex8(st_RtcTime.Hours);
+	uartEchoStr(":");
+	uartEchoHex8(st_RtcTime.Minutes);
+	uartEchoStr(":");
+	uartEchoHex8(st_RtcTime.Seconds);
+	uartEchoStr("(");
+	uartEchoHex8((uint8_t)st_RtcTime.SubSeconds);
+	uartEchoStrln(")");
+}
+
+/**
+  * @brief RTC日付を更新する
+  * @param pu8_BcdData BCDデータのポインタ
+  * @retval None
+  */
+static void updateRtcDate(uint8_t *pu8_BcdData)
+{
+	RTC_DateTypeDef st_RtcDate = {0};
+
+	st_RtcDate.Year = pu8_BcdData[0];
+	st_RtcDate.Month = pu8_BcdData[1];
+	st_RtcDate.Date = pu8_BcdData[2];
+	st_RtcDate.WeekDay = pu8_BcdData[3];
+	/* RTC日付を設定 */
+	if (HAL_RTC_SetDate(&hrtc, &st_RtcDate, RTC_FORMAT_BCD) != HAL_OK) {
+		/* Setting Error */
+		Error_Handler();
+	}
+}
+
+/**
+  * @brief RTC時刻を更新する
+  * @param pu8_BcdData BCDデータのポインタ
+  * @retval None
+  */
+static void updateRtcTime(uint8_t *pu8_BcdData)
+{
+	RTC_TimeTypeDef st_RtcTime = {0};
+
+	st_RtcTime.Hours = pu8_BcdData[0];
+	st_RtcTime.Minutes = pu8_BcdData[1];
+	st_RtcTime.Seconds = pu8_BcdData[2];
+	st_RtcTime.SubSeconds = pu8_BcdData[3];
+	st_RtcTime.DayLightSaving = RTC_DAYLIGHTSAVING_NONE;
+	st_RtcTime.StoreOperation = RTC_STOREOPERATION_RESET;
+	/* RTC時刻を設定 */
+	if (HAL_RTC_SetTime(&hrtc, &st_RtcTime, RTC_FORMAT_BCD) != HAL_OK) {
+		/* Setting Error */
+		Error_Handler();
+	}
+}
+
+/**
+  * @brief 文字列をBCDデータに変換する
+  * @param ps8_StrData 文字列の先頭ポインタ
+  * @param u16_StrLen 文字列の長さ
+  * @param pu8_BcdData BCDデータのポインタ
+  * @retval OK/NG
+  */
+static uint8_t convStrToBcd(const uint8_t *ps8_StrData, uint16_t u16_StrLen, uint8_t *pu8_BcdData)
+{
+	uint16_t u16_Index;
+	uint16_t u16_BcdIndex;
+	uint8_t u8_RetCode = OK;
+
+	/* 文字列の長さがRTC文字列サイズを超える場合 */
+	if (u16_StrLen > RTC_STR_SIZE) {
+		return NG;
+	}
+
+	for (u16_Index = 0; u16_Index < u16_StrLen; u16_Index++) {
+		u16_BcdIndex = u16_Index / 2;
+		if ((ps8_StrData[u16_Index] >= '0') && (ps8_StrData[u16_Index] <= '9')) {
+			pu8_BcdData[u16_BcdIndex] = (pu8_BcdData[u16_BcdIndex] << 4) + ps8_StrData[u16_Index] - '0';
+		}
+		else {
+			u8_RetCode = NG;
+			break;
+		}
+	}
+	return u8_RetCode;
 }
