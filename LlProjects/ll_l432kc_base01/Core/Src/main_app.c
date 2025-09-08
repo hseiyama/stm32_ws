725f13c23d37182cf1bd1b57f8f2d788b54e15b5
Hirofumi Seiyama <hseiyama@gmail.com>
2025-09-08 23:09:45 +0900

    ll_l432kc_base01: UART命令を追加

--- LlProjects/ll_l432kc_base01/Core/Src/main_app.c
+++ LlProjects/ll_l432kc_base01/Core/Src/main_app.c
@@ -16,12 +16,17 @@
 #define TIME_1S				(1000)					/* 1秒判定時間[ms]			*/
 #define UART_BUFF_SIZE		(58)					/* UARTバッファサイズ		*/
 
+/* UART命令 */
+#define UART_CMD_HELP		(0x08)					/* ヘルプ表示(^H)			*/
+#define UART_CMD_RESET		(0x12)					/* リセット(^R)				*/
+#define UART_CMD_SLEEP		(0x13)					/* スリープ(^S)				*/
+
 /* Private macro -------------------------------------------------------------*/
 
 /* Private variables ---------------------------------------------------------*/
 static Timer sts_Timer1s;							/* 1秒タイマー				*/
 static uint8_t u8s_RcvData[UART_BUFF_SIZE];			/* UART受信データ			*/
-static uint16_t u16_RcvDataSize;					/* UART受信データサイズ		*/
+static uint16_t u16s_RcvDataSize;					/* UART受信データサイズ		*/
 
 
 /* Private function prototypes -----------------------------------------------*/
@@ -47,12 +52,13 @@ void EXTI12_Rising_Callback(void)
 void setup(void)
 {
 	mem_set08(&u8s_RcvData[0], 0x00, UART_BUFF_SIZE);
-	u16_RcvDataSize = 0;
+	u16s_RcvDataSize = 0;
 
 	/* タイマーを開始する */
 	startTimer(&sts_Timer1s);
 
 	/* プログラム開始メッセージを表示する */
+	uartEchoStrln("");
 	uartEchoStrln("Start UART/GPIO sample!!");
 }
 
@@ -64,11 +70,40 @@ void setup(void)
 void loop(void)
 {
 	/* UART受信データを取得する */
-	u16_RcvDataSize = uartGetRxData(&u8s_RcvData[0], UART_BUFF_SIZE);
+	u16s_RcvDataSize = uartGetRxData(&u8s_RcvData[0], UART_BUFF_SIZE);
 	/* UART受信データが存在する場合 */
-	if (u16_RcvDataSize > 0) {
+	if (u16s_RcvDataSize > 0) {
 		/* UART送信データを登録する */
-		uartSetTxData(&u8s_RcvData[0], u16_RcvDataSize);
+		uartSetTxData(&u8s_RcvData[0], u16s_RcvDataSize);
+
+		/* UART命令解析 */
+		switch (u8s_RcvData[0]) {
+		/* ヘルプ表示(^H) */
+		case UART_CMD_HELP:
+			/* UART命令表示 */
+			uartEchoStrln("");
+			uartEchoStrln("^H :Help");
+			uartEchoStrln("^R :Reset");
+			uartEchoStrln("^S :Sleep");
+			break;
+		/* リセット(^R) */
+		case UART_CMD_RESET:
+			/* リセット処理 */
+			NVIC_SystemReset();
+			break;
+		/* スリープ(^S) */
+		case UART_CMD_SLEEP:
+			/* SysTickタイマー停止 */
+			LL_SYSTICK_DisableIT();
+			/* イベント待機 */
+			__WFE();
+			__WFE();
+			/* SysTickタイマー開始 */
+			LL_SYSTICK_EnableIT();
+			/* 文字を出力する */
+			uartEchoStr("<Wakeup!!>");
+			break;
+		}
 	}
 
 	/* 1秒判定時間が満了した場合 */
